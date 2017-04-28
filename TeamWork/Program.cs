f48b41054478370e6f5c3e813bc793d4f20da919
luizaarakelyan <luiza-arakel@yandex.com>
2017-04-28 20:03:27 +0400

    last changes at ArmSoft

--- TeamWork/Program.cs
+++ TeamWork/Program.cs
@@ -10,7 +10,35 @@ namespace TeamWork
     {
         static void Main(string[] args)
         {
-
+            Cafe cafe1 = new Cafe("ALD", "Nalbandyan 14", "0552365980", new TimeSpan(9, 0, 0), new TimeSpan(22, 0, 0));
+            User activeUser = new User("Luiza", "Arakelyan", 17);
+            bool status = true;
+            while (status)
+            {
+                string s = Console.ReadLine();
+                switch (s)
+                {
+                    case "Rate":
+                        Console.WriteLine("Cafe name: ");
+                        string cafeName = Console.ReadLine();
+                        Console.WriteLine("Enter your rate: ");
+                        string cafeRate = Console.ReadLine();
+                        for (int i = 0; i < Cafe.menu_am.Count; i++)
+                        {
+                            if (cafeName.Equals(Cafe.menu_am[i].Name))
+                                Cafe.menu_am[i].Rate(int.Parse(cafeRate), activeUser);
+                        }
+                        Console.WriteLine(cafe1.ToString());
+                        break;
+                    case "Exit":
+                        status = false;
+                        break;
+                    default:
+                        Console.WriteLine("It is not command");
+                        break;
+                        
+                }
+            }
         }
     }
 }
--- TeamWork/User.cs
+++ TeamWork/User.cs
@@ -8,11 +8,18 @@ namespace TeamWork
 {
     class User: Person
     {
+        public int ID { get; private set; }
+        private static int idCount = 1;
         List<Cafe> favorite = new List<Cafe>();
+        List<User> users = new List<User>();
 
         public User(string name, string lastname, byte age ): base(name, lastname, age)
-        { }
+        {
+            users.Add(this);
+            this.ID = idCount;
+            idCount++;
+        }
 
-
+   
     }
 }
