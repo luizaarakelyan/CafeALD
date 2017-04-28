04a4c5586590192e3698543b4087feec07caf594
Admin <Admin@DESKTOP-KDDHN9G>
2017-04-28 11:22:22 +0400

    Добавьте файлы проекта.

---
+++ TeamWork/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamWork
+{
+    class User: Person
+    {
+        List<Cafe> favorite = new List<Cafe>();
+
+        public User(string name, string lastname, byte age ): base(name, lastname, age)
+        { }
+
+
+    }
+}
